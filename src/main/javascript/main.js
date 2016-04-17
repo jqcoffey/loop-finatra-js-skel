436ce55fb832990acbf5790ae9910277a79a58d7
Justin Coffey <j.coffey@criteo.com>
2016-04-17 19:57:15 +0200

    add react components that call /query

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -2,7 +2,62 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+class DataRow extends React.Component {
+    render() {
+        let data = JSON.parse(this.props.data);
+        let row = [];
+        data.forEach(_ => row.push(<td>{_}</td>));
+
+        return (<tr>{row}</tr>)
+    }
+}
+
+class DataTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {
+                data: [],
+                columns: []
+            }
+        };
+    }
+
+    componentDidMount() {
+        fetch('/query', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({'name': 'rates', 'params': []})
+        }).then(res => res.json())
+            .then(data => this.setState({data}));
+    }
+
+    render() {
+        if (this.state.data) {
+            let data = this.state.data.data;
+            let columns = this.state.data.columns;
+
+            let rows = [];
+            data.forEach(_ => rows.push(<DataRow data={JSON.stringify(_)}/>));
+
+            let header = [];
+            columns.forEach(_ => header.push(<th>{_}</th>));
+
+            return (
+                <table>
+                    <thead>
+                    <tr>
+                        {header}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {rows}
+                    </tbody>
+                </table>
+            )
+
+        }
+    }
+}
+
+ReactDOM.render(<DataTable />, document.getElementById('example'));
